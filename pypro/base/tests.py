806e556975ba31e6feaa927ae04820055569ab5c
maldonadopereira <dmaldonadopereira@gmail.com>
2022-01-30 14:56:07 -0300

    Configurado Pytest do Django

--- pypro/base/tests.py
+++ pypro/base/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+def test_status_code(client:Client):
+    resp = client.get('/')
+    assert resp.status_code == 200
+
+
