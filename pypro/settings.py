f2f3cece16f5e83a4ebb2172017ce3d6f1acd386
maldonadopereira <dmaldonadopereira@gmail.com>
2022-02-02 21:58:47 -0300

    Incluido o pytest-django no workflow do github actions1

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=yk@q2(p6p@gsk1f90c5&rp9(v_3#iw!_yn%n744guwb^7@bfp'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast = Csv())
+#['pyprodavid.herokuapp.com']
 
 
 # Application definition
